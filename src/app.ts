58d609951a0e76b35300d2437d6c5f24951b4ff7
Immanuel Onum <immanuel.o.onum@gmail.com>
2021-11-13 10:11:55 +0100

    added cors middleware

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,13 @@ import log from "./logger";
 import connect from './db/connect'
 import routes from './routes'
 import { deserializeUser } from "./middleware";
+import enableCors from './middleware/enableCors';
 
 const port = config.get('port') as number;
 const host = config.get('host') as string;
 
 const app = express();
+app.use(enableCors);
 app.use(deserializeUser)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
